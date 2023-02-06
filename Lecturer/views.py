db7091114144dbd28bd7d5d0f7d6ec778d7871cf
The Meta <alexmeta517@gmail.com>
2023-02-06 15:24:51 +0300

    Folium Heat Map

--- Lecturer/views.py
+++ Lecturer/views.py
@@ -431,7 +431,7 @@ def folStud(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         print('ajax')
 
-        inst = request.POST.get('heat')
+        inst = request.POST.get('inst')
 
         #print(inst)
 
@@ -451,10 +451,25 @@ def folStud(request):
 
             m = m._repr_html_()
 
-            print(m)
+            return JsonResponse({'data': m}) 
+
+        elif inst == 'Pins':
+            coordinates = []
+
+            for one_location in all_locations:
+
+
+                coordinate = [one_location.latitude,one_location.longitude]
+
+                coordinates.append(coordinate)
+
+                folium.Marker(location=[one_location.latitude, one_location.longitude]).add_to(m)
+
+            m = m._repr_html_()
 
             return JsonResponse({'data': m}) 
 
+            
     else:
         print('not ajax')
         #Now we get all tye company locations
--- Static/Js/Lecturer/Dashboard/folStud.js
+++ Static/Js/Lecturer/Dashboard/folStud.js
@@ -1,10 +1,13 @@
 //Lets get all the neccessary items
 let heatBtn = document.getElementById('heat_Map');
+let pinBtn = document.getElementById('pins_Map');
+
 let mapSelf = document.getElementById('mapSelf');
 
 let csrf = document.getElementsByName('csrfmiddlewaretoken');
 
 //Add Event listiner click
+//Heat
 heatBtn.addEventListener('click', ()=> {
     console.log('click')
 
@@ -13,7 +16,40 @@ heatBtn.addEventListener('click', ()=> {
 
     let formData = new FormData();
 
-    formData.append('heat','Heat');
+    formData.append('inst','Heat');
+
+    formData.append('csrfmiddlewaretoken', csrf[0].value);
+
+    $.ajax({
+        type:'POST',
+        url:'/lecturer/studFol/',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response){
+
+            console.log(response.data)
+
+            mapSelf.innerHTML = response.data
+            
+        },
+        error: function(error){
+            
+        }
+    });
+
+});
+
+//Pins
+pinBtn.addEventListener('click', ()=> {
+    console.log('click')
+
+    //Remove the map
+    mapSelf.innerHTML = '';
+
+    let formData = new FormData();
+
+    formData.append('inst','Pins');
 
     formData.append('csrfmiddlewaretoken', csrf[0].value);
 
