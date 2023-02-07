dd2aa46ae271e80d0cb4147568197a89af9f655d
The Meta <alexmeta517@gmail.com>
2023-02-07 20:32:33 +0300

    Student Category Folium

---
+++ Static/Js/Student/Dashboard/folCat.js
@@ -0,0 +1,12 @@
+//Lets get all the important items
+let catBtns = document.getElementsByClassName('change_map');
+
+for(let a = 0; a < catBtns.length; a++){
+
+    catBtns[a].addEventListener('click', ()=> {
+
+        
+
+    })
+
+}
